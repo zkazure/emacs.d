48559bc9f3f1de83268d3290880efa08bf1f05a2
Kazure Zheng <kazurezheng@gmail.com>
2026-08-30 19:32:45 +0800

    chore(compile): some modify

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -1,14 +1,12 @@
 ;; -*- lexical-binding: t; -*-
 
-(global-set-key (kbd "C-c c C") 'compile)
+(global-set-key (kbd "C-c c c") 'compile)
 (global-set-key (kbd "<f6>") 'recompile)
 
 
 ;; ansi color
 (require 'ansi-color)
-(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
-
-(setq-default compilation-scroll-output t)
+(setq compilation-scroll-output t)
 (add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
 
