6f51b04e57515d1b970eee9baef102aba98fd536
Kazure Zheng <kazurezheng@gmail.com>
2026-09-09 09:26:45 +0800

    feat(vertico): from posframe to buffer-frame

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -1,15 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
 (require 'vertico)
-(add-to-list 'load-path
-             (expand-file-name "lib/vertico/extensions/" user-emacs-directory))
 (require 'vertico-buffer)
 (require 'vertico-directory)
 (require 'vertico-flat)
 (require 'vertico-grid)
 (require 'vertico-indexed)
 ;; (require 'vertico-mouse)
-(require 'vertico-multiform)
+;; (require 'vertico-multiform)
 ;; (require 'vertico-quick)
 ;; (require 'vertico-repeat)
 ;; (require 'vertico-reverse)
@@ -28,43 +26,9 @@
 
 (vertico-indexed-mode)
 
-
-(require 'vertico-posframe)
-(defun my-vertico-posframe-truncate-line (line)
-  "Truncate Vertico candidate LINE without affecting minibuffer input."
-  (let* ((width (- vertico-posframe-width 2))
-         ;; Vertico 的每个候选末尾自带 \n，
-         ;; 单独保存它，避免破坏候选的行结构和 face。
-         (newline (if (string-suffix-p "\n" line)
-                      (substring line -1)
-                    ""))
-         (body (if (string-suffix-p "\n" line)
-                   (substring line 0 -1)
-                 line)))
-    (concat
-     (truncate-string-to-width body width 0 nil "…")
-     newline)))
-
-(defun my-vertico-posframe-truncate-candidates (lines)
-  "Truncate candidate lines when using wrapping vertico-posframe."
-  (if (and vertico-posframe-mode
-           (not vertico-posframe-truncate-lines)
-           (numberp vertico-posframe-width))
-      (mapcar #'my-vertico-posframe-truncate-line lines)
-    lines))
-
-(advice-add 'vertico--arrange-candidates
-            :filter-return
-            #'my-vertico-posframe-truncate-candidates)
-
-(setq vertico-posframe-poshandler #'posframe-poshandler-frame-top-center
-      vertico-posframe-truncate-lines nil
-      )
-
-(setq vertico-multiform-categories
-      '((t posframe)))
-(vertico-multiform-mode)
-
+(require 'vertico-buffer-frame)
+(setq vertico-buffer-frame-auto-width t)
+(vertico-buffer-frame-mode 1)
 
 
 ;; icons
