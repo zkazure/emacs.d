2371aa5945957d013e9cd9c47b411d4cb67c7f3a
Kazure Zheng <kazurezheng@gmail.com>
2026-09-12 02:14:36 +0800

    refactor: streamline standalone mini configuration

--- init-mini.el
+++ init-mini.el
@@ -1,52 +1,25 @@
-;; Load path  -*- lexical-binding: t; -*-
+;;; init-mini.el --- Standalone built-in Emacs configuration -*- lexical-binding: t; -*-
 
-;; (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
-(push (expand-file-name "lisp" user-emacs-directory) load-path)
+;;; Commentary:
+;; A small, self-contained entry point.  It deliberately uses only Emacs
+;; built-ins and does not load the modular main configuration.
 
-;; Packages
-;; Without this comment Emacs25 adds (package-initialize) here
+;;; Code:
+
+;; Load path
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Package metadata.  This file neither initializes packages nor installs them.
 (setq package-archives
-      '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")))
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
 
 ;; Better defaults
-(setq inhibit-splash-screen t)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets) ; Show path if names are same
-(setq delete-by-moving-to-trash t)         ; Deleting files go to OS's trash folder
-(setq set-mark-command-repeat-pop t)       ; Repeating C-SPC after popping mark pops it again
-
-;; Tab and Space
-;; Permanently indent with spaces, never with TABs
-(setq-default c-basic-offset   4
-              tab-width        4
-              indent-tabs-mode nil)
-
-;; UI
-(load-theme 'wombat t)
-(set-face-attribute 'default nil :height 120)
-
-
-(menu-bar-mode -1)
-
-(if (fboundp 'display-line-numbers-mode)
-    (progn
-      (add-hook 'prog-mode-hook #'display-line-numbers-mode)))
-
-;; Basic modes
-(show-paren-mode 1)
-(global-auto-revert-mode 1)
-
-(electric-pair-mode 1)
-(electric-indent-mode 1)
-
-(global-set-key (kbd "M-p") #'backward-paragraph)
-(global-set-key (kbd "M-n") #'forward-paragraph)
-
-;; Completion
-(when (fboundp 'global-completion-preview-mode)
-  (global-completion-preview-mode 1))
-
-(setq save-interprogram-paste-before-kill t
+(setq inhibit-splash-screen t
+      uniquify-buffer-name-style 'post-forward-angle-brackets
+      delete-by-moving-to-trash t
+      set-mark-command-repeat-pop t
+      save-interprogram-paste-before-kill t
       apropos-do-all t
       mouse-yank-at-point t
       require-final-newline t
@@ -55,12 +28,50 @@
       read-buffer-completion-ignore-case t
       completion-ignore-case t
       ediff-window-setup-function 'ediff-setup-windows-plain
-      custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load-file custom-file))
-
-(setq dabbrev-abbrev-char-regexp "[A-Za-z0-9_-]"
+      dabbrev-abbrev-char-regexp "[A-Za-z0-9_-]"
       dabbrev-case-fold-search nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Init-mini.el ends here
+;; Tab and space
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode nil)
+
+;; UI
+(load-theme 'wombat t)
+(set-face-attribute 'default nil :height 120)
+(menu-bar-mode -1)
+
+(when (fboundp 'display-line-numbers-mode)
+  (add-hook 'prog-mode-hook #'display-line-numbers-mode))
+
+;; Basic modes
+(show-paren-mode 1)
+(global-auto-revert-mode 1)
+(electric-pair-mode 1)
+(electric-indent-mode 1)
+
+(global-set-key (kbd "M-p") #'backward-paragraph)
+(global-set-key (kbd "M-n") #'forward-paragraph)
+
+;; Built-in minibuffer completion
+(setq completion-styles '(basic partial-completion substring)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles partial-completion))))
+(when (fboundp 'fido-vertical-mode)
+  (fido-vertical-mode 1))
+(when (fboundp 'global-completion-preview-mode)
+  (global-completion-preview-mode 1))
+
+;; Persist useful navigation history without loading external packages.
+(savehist-mode 1)
+(save-place-mode 1)
+
+;; Load user customizations normally so errors remain visible.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file nil 'nomessage))
+
+
+(provide 'init-mini)
+
+;;; init-mini.el ends here
